483cd0c94ccfde8c11eb42d5d9161b08491ffd28
tusharsankhala <tusharsankhala@gmail.com>
2021-01-29 18:56:32 +0530

    Simple Vulkan Extension Query

---
+++ Engine/Engine.cpp
@@ -0,0 +1,47 @@
+#define GLFW_INCLUDE_INCLUDE
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
+#include <vulkan/vulkan.hpp>
+
+#include <vector>
+#include <iostream>
+
+int main()
+{
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan Window", nullptr, nullptr);
+
+    glfwMakeContextCurrent(window);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::vector<VkExtensionProperties> extensions(extensionCount);
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+    std::cout << "Extensions Supported: " << extensionCount;
+    for (auto extension : extensions)
+    {
+        std::cout << extension.extensionName << '\n';
+    }
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    return 0;
+
+}
+
